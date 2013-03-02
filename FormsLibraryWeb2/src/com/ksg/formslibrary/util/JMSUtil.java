a1f95ccf99bae811276da23d7c7fd8db30ca9bb7
Rodney Odvina <rodvina@comcast.net>
2013-03-02 17:03:34 -0500

    Initial Commit

---
+++ FormsLibraryWeb2/src/com/ksg/formslibrary/util/JMSUtil.java
@@ -0,0 +1,11 @@
+package com.ksg.formslibrary.util;
+
+import com.ksg.formslibrary.domain.SearchCriteria;
+
+public class JMSUtil {
+
+	public static String convert(SearchCriteria searchCriteria) {
+		
+		return "";
+	}
+}
